ccaac2b5501f7b2b748fcf4f19ccee4368ef5af1
Alexandr Harabara <alexandr.harabara@bitpanda.com>
2022-04-08 18:51:47 +0300

    - introduced database driver  - implemented report saving for db  - implemented db deploy and import from yaml  - a lot of refactoring

--- quizler.php
+++ quizler.php
@@ -1,6 +1,10 @@
 #!/usr/bin/env php
 <?php
 
+const DB_PATH = __DIR__.'/storage/quizler.db';
+const DB_FOLDER_PATH = __DIR__.'/storage/';
+const QUIZZES_FOLDER_PATH = DB_FOLDER_PATH.'/quizzes';
+
 require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
@@ -12,6 +16,8 @@ $application->add(new \Quiz\Command\CreateQuizCommand());
 $application->add(new \Quiz\Command\SearchAnswerCommand());
 $application->add(new \Quiz\Command\AnswerStatsCommand());
 $application->add(new \Quiz\Command\ToDoCommand());
+$application->add(new \Quiz\Command\GenerateFromCommand());
+$application->add(new \Quiz\Command\DeployCommand());
 $application->run();
 
 // choice of quiz
