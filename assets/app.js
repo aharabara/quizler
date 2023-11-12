1a717d64981c82726dab9877c33d778eb9018906
aharabara <aharabara@pentalog.com>
2023-11-12 23:48:54 +0200

    Migration to Turbo UI. - disabled stimulus controllers

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,7 @@
-// import {app} from './bootstrap.js';
+import './bootstrap.js';
 
 import "./core-extensions";
 import "./global-listeners";
-
-// window.Stimulus = app;
-
 import "highlight.js/styles/androidstudio.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
